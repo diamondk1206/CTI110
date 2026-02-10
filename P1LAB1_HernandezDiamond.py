70ac71472893e88d42d48718687f116472bd36a9
diamondk1206 <hernandd2265@student.faytechcc.edu>
2026-02-10 11:05:55 -0500

    Add files via upload

---
+++ P1LAB1_HernandezDiamond.py
@@ -0,0 +1,15 @@
+# Diamond Hernandez
+# 02/10/2026
+# P1LAB1
+# Create program that allows user to enter name
+
+# User enters first name
+name = input("Enter your first name: ")
+
+# User enters last name
+
+user_name = input("Enter your last name: ")
+
+# Add welcome message
+print ("Hello", name, user_name,"!", end=' ')
+print("Welcome to CTI-110")
