481d1f58e0be964735b8c71565a4ebec9b6b977b
diamondk1206 <hernandd2265@student.faytechcc.edu>
2026-02-12 10:49:06 -0500

    Add files via upload

---
+++ P1HW1_HernandezDiamond.py
@@ -0,0 +1,33 @@
+# Diamond Hernandez
+# 02/10/2026
+# P1HW1
+# Converting user input
+
+# Get integers from user
+print("-----Calculating Exponents-----")
+print()
+print()
+value1= int(input ("Enter an integer as the base value: "))
+value2= int(input ("Enter an integer as the exponent: "))
+print()
+print()
+# Calculating result
+result= value1 ** value2
+print(value1, "raised to the power of", value2, "is", result, "!!")
+
+print()
+print()
+print("-----Addition and Subtraction-----")
+
+print()
+print()
+# Get integers from user
+value3= int(input("Enter a starting integer: "))
+value4= int(input ("Enter an integer to add: "))
+value5= int(input ("Enter an integer to subtract: "))
+
+print()
+print()
+# Calculating result
+result2= value3 + value4 - value5
+print(value3, "+", value4, "-", value5, "is equal to", result2)
